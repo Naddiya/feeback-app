44555c95968a5e5e761ee0bed5c07e0fde556165
Naddiya <nadiamedkouriz@gmail.com>
2022-03-01 14:58:18 +0100

    feat: conditional jsx review

--- src/App.js
+++ src/App.js
@@ -9,21 +9,28 @@ function App() {
         { id: 3, text: 'comment three' },
         { id: 4, text: 'comment four' }
     ];
+    const loading = false;
+    const showComments = true;
+    const commentBlock = (
+        <div className="comments">
+            <h3>Comments ({comments.length})</h3>
+            <ul>
+                {comments.map((comment, index) => (
+                    <li key={index}>{comment.text}</li>
+                ))}
+            </ul>
+        </div>
+    );
+
+    if (loading) return <h1>Loading ...</h1>;
 
     return (
         <div>
             <h1>{title}</h1>
             <p>{body}</p>
-            {Math.random() * 5 + 5}
+            {showComments && commentBlock}
 
-            <div className="comments">
-                <h3>Comments ({comments.length})</h3>
-                <ul>
-                    {comments.map((comment, index) => (
-                        <li key={index}>{comment.text}</li>
-                    ))}
-                </ul>
-            </div>
+
         </div>
     );
 }
