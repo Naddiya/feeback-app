0d424367659112122978ca8e35eff6474f1985a6
Naddiya <nadiamedkouriz@gmail.com>
2022-03-03 15:33:37 +0100

    feat: Custom button component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Card from './shared/Card';
+import Button from './shared/Button';
 
 
 const FeedbackForm = () => {
@@ -19,7 +20,7 @@ const FeedbackForm = () => {
                     placeholder="Write a review"
                     onChange={handleTextChange}
                 />
-                <button type="submit">Send</button>
+                <Button type="submit">Send</Button>
             </div>
         </Card>
     );
