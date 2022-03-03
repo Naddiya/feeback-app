a42e7123be17189ee74fb9af45c4fcf4646e82f8
Naddiya <nadiamedkouriz@gmail.com>
2022-03-03 12:40:44 +0100

    feat: conditionnal styling

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,18 @@
+import Card from './shared/Card';
+import PropTypes from 'prop-types';
+
 const FeedbackItem = ({ item }) => {
     return (
-        <div className="card">
+        <Card>
             <div className="num-display">{item.rating}</div>
             <div className="text-display">{item.text}</div>
-        </div>
+        </Card>
     );
 };
+
+FeedbackItem.protoTypes = {
+    item: PropTypes.object.isRequired,
+    
+}
 
 export default FeedbackItem;
