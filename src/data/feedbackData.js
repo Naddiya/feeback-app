77ab8d16ea1a88c13e1095b3f53c588de5296edf
Naddiya <nadiamedkouriz@gmail.com>
2022-03-03 14:26:53 +0100

    feedbackStats component & reactivity

--- src/data/feedbackData.js
+++ src/data/feedbackData.js
@@ -11,7 +11,7 @@ const feedbackData = [
     },
     {
         id: 3,
-        rating: 0,
+        rating: 1,
         text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem cumque eius perspiciatis tempora quas fuga delectus ullam similique? Neque nemo, numquam eius temporibus eaque placeat fuga nostrum consequatur asperiores maxime.'
     },
 ];
