ffcaf13fb1998614f8604e248523008963ec6482
Naddiya <nadiamedkouriz@gmail.com>
2022-03-04 18:23:53 +0100

    feat: addFeedback + set proxy

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,32 +1,40 @@
 import { createContext, useEffect, useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
 export const FeebackProvider = ({ children }) => {
-    const [isLoading, setIsLoading] = useState(true)
-    const [feedback, setFeedback] = useState([])
+    const [isLoading, setIsLoading] = useState(true);
+    const [feedback, setFeedback] = useState([]);
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     });
 
     useEffect(() => {
-        fetchFeedback()
-    }, [])
+        fetchFeedback();
+    }, []);
 
     const fetchFeedback = async () => {
-        const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`);
-        const data = await response.json()
+        const response = await fetch('/feedback?_sort=id&_order=desc');
+        const data = await response.json();
 
-        setFeedback(data)
-        setIsLoading(false)
+        setFeedback(data);
+        setIsLoading(false);
     };
 
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4();
-        setFeedback([newFeedback, ...feedback]);
+    const addFeedback = async (newFeedback) => {
+        const response = await fetch('/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body : JSON.stringify(newFeedback)
+        })
+
+        const data = await response.json();
+        setFeedback([data, ...feedback]);
     };
+
 
     const deleteFeedback = (id) => {
         if (window.confirm('Are you sur you want to delete ?')) {
