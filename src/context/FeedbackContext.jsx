e986a552b922e1f286cca07b1276d80bdf0a4242
Naddiya <nadiamedkouriz@gmail.com>
2022-03-04 17:15:41 +0100

    feat: fetch data from api

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,30 +1,27 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
 export const FeebackProvider = ({ children }) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            rating: 10,
-            text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'
-        },
-        {
-            id: 2,
-            rating: 8,
-            text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'
-        },
-        {
-            id: 3,
-            rating: 1,
-            text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'
-        },
-    ]);
+    const [isLoading, setIsLoading] = useState(true)
+    const [feedback, setFeedback] = useState([])
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     });
+
+    useEffect(() => {
+        fetchFeedback()
+    }, [])
+
+    const fetchFeedback = async () => {
+        const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`);
+        const data = await response.json()
+
+        setFeedback(data)
+        setIsLoading(false)
+    };
 
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4();
